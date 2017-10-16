2dcd5e510d0432f422fa126f05fa3496f3cd2148
mohamed hussein <m.hussein1389@gmail.com>
2017-10-16 11:18:32 +0200

    implement printForward function

---
+++ doublyLinkedList/doublylinkedlist.c
@@ -0,0 +1,26 @@
+#include"doublylinkedlist.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void printForward(nodePtr head)
+{
+    //check if list is empty
+    if(head == NULL)
+    {
+        puts("an empty list");
+      return;
+    }
+     //traverse pointer
+    nodePtr p =  head;
+    printf("[ ");
+    while(p!= NULL)
+    {
+        //get data
+        int data = p->data;
+        //print data
+        printf("%d ",data);
+        //move pointer
+        p = p->next;
+    }
+    printf(" ]\n");
+}
