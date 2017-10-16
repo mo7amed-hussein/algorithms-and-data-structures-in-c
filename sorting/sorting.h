385a7f7dae433cdcd2ffd99eb0d1915720b2482d
mohamed hussein <m.hussein1389@gmail.com>
2017-10-16 21:17:09 +0200

    add sorting functions prototypes

---
+++ sorting/sorting.h
@@ -0,0 +1,46 @@
+#ifndef __SORTING__
+#define __SORTING__
+
+/**
+ * @brief sort an array using bubble sort
+ * @param int[] array
+ * @param int array length
+ */
+void sortBubble(int arr[],int len);
+
+/**
+ * @brief sort an array using selection sort
+ * @param int[] array
+ * @param int array length
+ */
+void sortSelect(int arr[],int len);
+
+/**
+ * @brief sort an array using insertion sort
+ * @param int[] array
+ * @param int array length
+ */
+void sortInsert(int arr[],int len);
+
+/**
+ * @brief sort an array using merge sort
+ * @param int[] array
+ * @param int array length
+ */
+void sortMerge(int arr[],int len);
+
+/**
+ * @brief sort an array using shelf sort
+ * @param int[] array
+ * @param int array length
+ */
+void sortShelf(int arr[],int len);
+
+/**
+ * @brief sort an array using quick sort
+ * @param int[] array
+ * @param int array length
+ */
+void sortQuick(int arr[],int len);
+
+#endif // __SORTING__
